eb2494389c95e9948d55130bbc0f63510b20cb65
Curt Caldwell <curt.caldwell24@gmail.com>
2019-03-20 12:37:58 -0700

    changed tool bar name

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         mLocationEditText = findViewById(R.id.locationEditText);
         mFindRestaurantsButton = findViewById(R.id.findRestaurantsButton);
 
+        
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
